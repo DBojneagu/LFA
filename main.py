c21830de5414417cbf2ae256ae146dfe65ee84bc
DBojneagu <david.bojneagu@yahoo.com>
2022-04-03 13:14:46 +0300

    update

--- main.py
+++ main.py
@@ -15,12 +15,44 @@ class Graph:
 
     def dfs(self,curr_node,viz= {}):
         viz[curr_node] = True
+        print(viz)
         for next_node,simbol in self.edges.get(curr_node,[]):
               if next_node in viz:
                  continue
               print(f"de la {curr_node} la {next_node} prin {simbol}")
+              print(self.edges[curr_node])
               self.dfs(next_node,viz)
 
+    def dfs2(self,curr_node,cuv,ok = 1,viz= {}):
+        viz[curr_node] = True
+        print(viz)
+        print(cuv)
+        while len(cuv) - 1 > 0 :
+            i = 0 
+            for next_node,simbol in self.edges.get(curr_node,[]):
+                i+=1 
+                print(self.edges[curr_node])
+
+                if simbol == cuv[0]:
+                    print(f"Am gasit litera, trecem mai departe,mergem prin {next_node}")
+                    cuv = cuv[1:]
+                    if len(cuv) == 1:
+                        print("Am gasit cuvantul, am terminat.")
+                        break
+                    self.dfs2(next_node,cuv)
+                    break    
+                if i == 3 and simbol != cuv[0]:
+                   print("Cuvantul nu poate fi acceptat") 
+                   break
+            break 
+        
+                 
+
+
+
+        
+
+                        
 g = Graph()
 f = open("input.txt")
 t = f.readline()
@@ -33,10 +65,9 @@ for i in range(nr_drumuri+1):
     aux = linie.split()
     g.add_edge(int(aux[0]), int(aux[1]), aux[2])
     
-
 g.print()
 print()
-g.dfs(1)
+g.dfs(0)
 
 stare_initiala = f.readline()
 pre_stare_finala = f.readline()
@@ -45,10 +76,14 @@ stare_finala = aux[1]
 nr_teste = f.readline()
 
 
+for linie in f:
+    cuv = linie 
+    print(f"Cuvantul este :{cuv}")
+    print(f"Numarul de teste {nr_teste} ")
+    print(f"Cuvantul initial: {cuv}")
+    g.dfs2(0,cuv)
 
 
-#for teste in range(nr_teste+1):
-     #cuv = f.readline()
 
 
 
