b13ed41ae695fec909a16a204a51ff65de792a07
DBojneagu <93039914+DBojneagu@users.noreply.github.com>
2022-04-06 11:16:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,25 +11,25 @@ class Graph:
 
             self.edges[from_node] = [(to_node, simbol)]
 
-    def dfs2(self, curr_node, cuv, L, lista_nr_noduri, L2, str_finala):
+    def dfs2(self, curr_node, cuv, Traseu, lista_nr_noduri, Traseu_Moore, str_finala):
 
         while len(cuv) > 0:
             i = 0
             for next_node, simbol in self.edges.get(curr_node, []):
                 i += 1
                 if simbol == cuv[0]:
-                    L.append(next_node)
-                    L2.append(lista_nr_noduri[next_node])
+                    Traseu.append(next_node)
+                    Traseu_Moore.append(lista_nr_noduri[next_node])
                     cuv = cuv[1:]
                     if len(cuv) == 0 and next_node in str_finala:
                         print("DA")
-                        print("".join(L2))
-                        print("Traseu: ", *L)
+                        print("".join(Traseu_Moore))
+                        print("Traseu: ", *Traseu)
                         break
                     if len(cuv) == 0 and next_node not in  str_finala:
                         print("NU")
                         break
-                    self.dfs2(next_node, cuv, L, lista_nr_noduri, L2, str_finala)
+                    self.dfs2(next_node, cuv, Traseu, lista_nr_noduri, Traseu_Moore, str_finala)
                     break
                 if i == len(self.edges[curr_node]) and simbol != cuv[0]:
                     print("NU")
@@ -64,8 +64,8 @@ nr_teste = f.readline()
 
 for linie in f:
     cuv = linie.strip()
-    L = []
-    L2 = []
-    L.append(int(stare_initiala))
-    L2.append(lista_nr_noduri[int(stare_initiala)])
-    g.dfs2(0, cuv, L, lista_nr_noduri, L2, stari_finale)
+    Traseu = []
+    Traseu_Moore = []
+    Traseu.append(int(stare_initiala))
+    Traseu_Moore.append(lista_nr_noduri[int(stare_initiala)])
+    g.dfs2(0, cuv, Traseu, lista_nr_noduri, Traseu_Moore, stari_finale)
